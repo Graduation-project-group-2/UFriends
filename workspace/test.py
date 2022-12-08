7f16024cb5a5f045cf0e3f8e885e832e2661186e
minkyung <minkyung7365@gmail.com>
2022-12-09 02:18:41 +0900

    workspace

---
+++ workspace/test.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+# from numpy import dot
+# from numpy.linalg import norm
+from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer
+import print_answer
+
+print_answer.print_answer()
