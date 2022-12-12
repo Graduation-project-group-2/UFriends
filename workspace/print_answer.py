e3fa308afe11283ed387956b7a9d20636caacff0
minkyung <minkyung7365@gmail.com>
2022-12-12 18:21:19 +0900

    대화 반복 및 '종료' 입력 시 대화 종료

--- workspace/print_answer.py
+++ workspace/print_answer.py
@@ -10,39 +10,40 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 def print_answer():
     df = pd.read_csv('../data/train_df_embedding.csv')
-
-    print("user: ", end="")
-    text = input()
-
-    # 학습한 모델 사용해서 user_input에 대한 embedding 생성
     model = SentenceTransformer('jhgan/ko-sroberta-multitask')
-    embedding = model.encode(text)
 
-    # embedding 컬럼을 리스트로 변환 후 similarity 계산
-    sim = []
-    for i in range(len(df)):
-        temp = df.loc[i]['embedding']
-        temp = temp.strip("[""]")
-        abc = temp.split()
+    while True:
+        print("user: ", end="")
+        text = input()
 
-        f = lambda x: cosine_similarity([embedding], [x])
-        sim.append(f(abc).squeeze())
+        if text == '종료':
+            print("대화를 종료합니다.")
+            break
 
-    df['similarity'] = sim
-    df = df.astype({'similarity': 'float'})
+        # 학습한 모델 사용해서 user_input에 대한 embedding 생성
+        embedding = model.encode(text)
 
-    answer = df.loc[df['similarity'].idxmax()]
+        # embedding 컬럼을 리스트로 변환 후 similarity 계산
+        sim = []
+        for i in range(len(df)):
+            temp = df.loc[i]['embedding']
+            temp = temp.strip("[""]")
+            abc = temp.split()
 
-    print('구분: ', answer['sentiment'])
-    print('유사한 질문: ', answer['user'])
-    print('챗봇 답변: ', answer['chatbot'])
-    print('유사도: ', answer['similarity'])  # 1에 가까울수록 유사
+            f = lambda x: cosine_similarity([embedding], [x])
+            sim.append(f(abc).squeeze())
 
+        df['similarity'] = sim
+        df = df.astype({'similarity': 'float'})
 
-# 피클로 저장 안 하는 게 나을 듯
-# with open("print_answer.pkl", "wb") as f:
-#     pickle.dump(print_answer(), f)
+        answer = df.loc[df['similarity'].idxmax()]
+
+        print('구분: ', answer['sentiment'])
+        print('유사한 질문: ', answer['user'])
+        print('챗봇 답변: ', answer['chatbot'])
+        print('유사도: ', answer['similarity'])  # 1에 가까울수록 유사
+
 
 # function call
-print_answer()
+# print_answer()
 
