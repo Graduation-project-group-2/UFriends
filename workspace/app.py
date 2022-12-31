86175c1ae5bcd7c38a54a0a241d1c2af33ef08b2
minkyung <minkyung7365@gmail.com>
2022-12-31 14:46:52 +0900

    pz

--- workspace/app.py
+++ workspace/app.py
@@ -3,18 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
-=======
 
->>>>>>> ab329c4 (flask2212)
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
 
-<<<<<<< HEAD
-=======
 
->>>>>>> ab329c4 (flask2212)
 @app.route('/user/<userName>')
 def hello_user(userName):
   return 'Hello, %s'%(userName)
